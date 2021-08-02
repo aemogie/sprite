c1cebdae9c8fa7c81ba0f496407d0a9a7767963a
aemogie <theaemogie@gmail.com>
2021-08-02 23:49:41 +0530

    :arrow_up: opengl and openal dependencies.

--- gl/build.gradle.kts
+++ gl/build.gradle.kts
@@ -1,16 +1,39 @@
+import org.gradle.internal.os.OperatingSystem
+
 version = "0.1.0-SNAPSHOT-01"
 
-val lwjglVersion = "3.2.3"
+@Suppress("INACCESSIBLE_TYPE")
+val lwjglNatives = when (OperatingSystem.current()) {
+	OperatingSystem.LINUX   -> System.getProperty("os.arch").let {
+		if (it.startsWith("arm") || it.startsWith("aarch64"))
+			"natives-linux-${if (it.contains("64") || it.startsWith("armv8")) "arm64" else "arm32"}"
+		else
+			"natives-linux"
+	}
+	OperatingSystem.MAC_OS  -> "natives-macos"
+	OperatingSystem.WINDOWS -> System.getProperty("os.arch").let {
+		if (it.contains("64"))
+			"natives-linux${if (it.startsWith("aarch64")) "-arm64" else ""}"
+		else
+			"natives-linux-x86"
+	}
+	else -> throw Error("Unrecognized or unsupported Operating system. Please set \"lwjglNatives\" manually")
+}
+
 dependencies {
 	api(project(":utils"))
 	//region LWJGL
-	implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
-	implementation("org.lwjgl:lwjgl:$lwjglVersion")
-	implementation("org.lwjgl:lwjgl-glfw:$lwjglVersion")
-	implementation("org.lwjgl:lwjgl-stb:$lwjglVersion")
-	runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-windows")
-	runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = "natives-windows")
-	runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = "natives-windows")
+	implementation(platform("org.lwjgl:lwjgl-bom:3.2.3"))
+	implementation("org.lwjgl:lwjgl:3.2.3")
+	implementation("org.lwjgl:lwjgl-glfw:3.2.3")
+	implementation("org.lwjgl:lwjgl-stb:3.2.3")
+	implementation("org.lwjgl:lwjgl-opengl:3.2.3")
+	implementation("org.lwjgl:lwjgl-openal:3.2.3")
+	runtimeOnly("org.lwjgl:lwjgl:3.2.3:$lwjglNatives")
+	runtimeOnly("org.lwjgl:lwjgl-glfw:3.2.3:$lwjglNatives")
+	runtimeOnly("org.lwjgl:lwjgl-stb:3.2.3:$lwjglNatives")
+	implementation("org.lwjgl:lwjgl-opengl:3.2.3:$lwjglNatives")
+	implementation("org.lwjgl:lwjgl-openal:3.2.3:$lwjglNatives")
 	//endregion
 	implementation("org.joml:joml:1.10.1")
 }
