4364674ff0cefbac6297e5ed1cc07a73489f13e3
aemogie <theaemogie@gmail.com>
2021-08-03 14:35:23 +0530

    :arrow_up: fix LWJGL natives. and remove redundant dependency declaration in "utils."
    Signed-off-by: aemogie <theaemogie@gmail.com>

--- gl/build.gradle.kts
+++ gl/build.gradle.kts
@@ -2,36 +2,42 @@ import org.gradle.internal.os.OperatingSystem
 
 @Suppress("INACCESSIBLE_TYPE")
 val lwjglNatives = when (OperatingSystem.current()) {
-	OperatingSystem.LINUX   -> System.getProperty("os.arch").let {
+	OperatingSystem.LINUX -> System.getProperty("os.arch").let {
 		if (it.startsWith("arm") || it.startsWith("aarch64"))
 			"natives-linux-${if (it.contains("64") || it.startsWith("armv8")) "arm64" else "arm32"}"
 		else
 			"natives-linux"
 	}
-	OperatingSystem.MAC_OS  -> "natives-macos"
+	OperatingSystem.MAC_OS -> "natives-macos"
 	OperatingSystem.WINDOWS -> System.getProperty("os.arch").let {
 		if (it.contains("64"))
-			"natives-linux${if (it.startsWith("aarch64")) "-arm64" else ""}"
+			"natives-windows${if (it.startsWith("aarch64")) "-arm64" else ""}"
 		else
-			"natives-linux-x86"
+			"natives-windows-x86"
 	}
 	else -> throw Error("Unrecognized or unsupported Operating system. Please set \"lwjglNatives\" manually")
 }
 
+repositories {
+	mavenCentral()
+}
+
 dependencies {
+	println(lwjglNatives)
 	implementation(project(":utils"))
-	//region LWJGL
+	
 	implementation(platform("org.lwjgl:lwjgl-bom:3.2.3"))
+	
 	implementation("org.lwjgl:lwjgl:3.2.3")
+	implementation("org.lwjgl:lwjgl-assimp:3.2.3")
 	implementation("org.lwjgl:lwjgl-glfw:3.2.3")
-	implementation("org.lwjgl:lwjgl-stb:3.2.3")
-	implementation("org.lwjgl:lwjgl-opengl:3.2.3")
 	implementation("org.lwjgl:lwjgl-openal:3.2.3")
+	implementation("org.lwjgl:lwjgl-opengl:3.2.3")
+	implementation("org.lwjgl:lwjgl-stb:3.2.3")
 	runtimeOnly("org.lwjgl:lwjgl:3.2.3:$lwjglNatives")
+	runtimeOnly("org.lwjgl:lwjgl-assimp:3.2.3:$lwjglNatives")
 	runtimeOnly("org.lwjgl:lwjgl-glfw:3.2.3:$lwjglNatives")
+	runtimeOnly("org.lwjgl:lwjgl-openal:3.2.3:$lwjglNatives")
+	runtimeOnly("org.lwjgl:lwjgl-opengl:3.2.3:$lwjglNatives")
 	runtimeOnly("org.lwjgl:lwjgl-stb:3.2.3:$lwjglNatives")
-	implementation("org.lwjgl:lwjgl-opengl:3.2.3:$lwjglNatives")
-	implementation("org.lwjgl:lwjgl-openal:3.2.3:$lwjglNatives")
-	//endregion
-	implementation("org.joml:joml:1.10.1")
 }
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-	testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
 	api("org.jetbrains:annotations:21.0.1")
 	api("com.google.code.gson:gson:2.8.7")
 }
