a152a4052cee0bbc54f461edb1e9054caf8ae76c
aemogie <54412618+aemogie@users.noreply.github.com>
2021-10-21 13:55:22 +0530

    :elephant: gradle stuff! upgraded wrapper to version `7.2` specified the Java version to be used. added kotlin!
    Signed-off-by: aemogie <54412618+aemogie@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,39 @@
-group = "io.github.aemogie.timble"
-version = "0.1.0"
+import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 
-subprojects {
-	apply(plugin = "java-library")
+plugins { kotlin("jvm") version "1.5.31" }
+
+val javaVersion = JavaVersion.VERSION_16
+
+tasks.wrapper {
+	gradleVersion = "7.2"
+	distributionType = ALL
+}
+
+allprojects {
+	apply {
+		plugin(JavaLibraryPlugin::class)
+		plugin(KotlinPluginWrapper::class)
+	}
 	
-	group = rootProject.group
-	version = rootProject.version
+	group = "io.github.aemogie.timble"
+	version = "0.1.0"
+	
+	java {
+		sourceCompatibility = javaVersion
+		targetCompatibility = javaVersion
+		withSourcesJar()
+		withJavadocJar()
+	}
+	
+	kotlin.target.compilations.forEach {
+		it.kotlinOptions.jvmTarget = javaVersion.toString()
+	}
 	
 	repositories { mavenCentral() }
+	
+	dependencies {
+		implementation(kotlin("stdlib-jdk8"))
+		implementation(kotlin("reflect"))
+	}
 }
