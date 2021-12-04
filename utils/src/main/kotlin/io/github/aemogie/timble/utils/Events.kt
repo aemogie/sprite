e05c49782375d4f5df3d3cb6a19ddc7adf1ff6b5
aemogie <54412618+aemogie@users.noreply.github.com>
2021-12-04 20:23:03 +0530

    :package: fix visibility in `Events.kt`
    Signed-off-by: aemogie <54412618+aemogie@users.noreply.github.com>

--- utils/src/main/kotlin/io/github/aemogie/timble/utils/Events.kt
+++ utils/src/main/kotlin/io/github/aemogie/timble/utils/Events.kt
@@ -4,18 +4,22 @@ import kotlin.reflect.KClass
 
 open class Event // :smiley:
 
-open class EventNode {
+abstract class EventNode {
 	private val listeners = object : HashMap<KClass<out Event>, ArrayList<(Event) -> Boolean>>() {
 		override operator fun get(key: KClass<out Event>) = super.get(key).let {
 			it ?: ArrayList<(Event) -> Boolean>().apply { super.put(key, this) }
 		}
 	}
 	
-	fun <T : Event> fire(event: T) = listeners[event::class].all { it(event) }
+	protected open fun <T : Event> fire(event: T) = listeners[event::class].all { it(event) }
+	
 	@Suppress("UNCHECKED_CAST")
 	fun <T : Event> subscribe(clazz: KClass<T>, listener: (T) -> Boolean) = listener.also {
 		listeners[clazz] += it as (Event) -> Boolean
 	}
 }
 
-@Suppress("unused") object EventBus : EventNode()
+//singleton instance with widened visibility
+@Suppress("unused") object EventBus : EventNode() {
+	public override fun <T : Event> fire(event: T) = super.fire(event)
+}
